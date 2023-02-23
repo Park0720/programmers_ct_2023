20a41d6536623f6cfc5eebd83c31497f4b6a937b
ê¹git config --global user.name ê¹ <git config --global user.name >
2023-02-23 10:32:09 +0900

    각도기 문제 해결

--- src/test/java/com/ll/level0/P120807/Tests.java
+++ src/test/java/com/ll/level0/P120807/Tests.java
@@ -10,6 +10,18 @@ public class Tests {
     @Test
     @DisplayName("2,3 => -1")
     void t1(){
-        assertThat(new Solution().solution(2,3)).isEqualTo(-1);
+        assertThat(new Solution().solution(2, 3)).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("11,11 => 1")
+    void t2() {
+        assertThat(new Solution().solution(2, 3)).isEqualTo(-1);
+    }
+
+    @Test
+    @DisplayName("7, 99 => -1")
+    void t3() {
+        assertThat(new Solution().solution(2, 3)).isEqualTo(-1);
     }
 }
